d4628592fa27b52034bbf30fd18fc8a27244bd84
senaybee <senay_berihu@outlook.com>
2020-07-04 16:24:34 -0700

    Add unique ids to each joke posted

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var uniqid = require('uniqid');
 
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     if (err) return console.log(err);
@@ -15,8 +16,21 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     
     app.use(bodyParser.json());
 
+    
+    app.post('/jokes', function(req, res){
+        console.log('Posting joke!')
+        const newJoke = {_id: uniqid.time(),username: req.body.username ,setup : req.body.setup, punchline: req.body.punchline, likedBy : [], dislikedBy : []}
+        jokesCollection.insertOne(newJoke)
+        .then((result) => {
+            res.send()
+        })
+        .catch((error) => res.status(400).send());
+    });
+
+
+    
     app.post('/:username', function(req, res) {
-        console.log('Just got a get request to users!')
+        console.log('Logging user in!')
         usersCollection.findOne({_id : req.params.username})
         .then((result) => {
             if(req.body.password === result.password){
@@ -28,7 +42,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     });
 
     app.post('/users', function(req, res) {
-        console.log('Just got a post request to users!')
+        console.log('Creating user account!')
         const newUser = {_id : req.body.username, password: req.body.password};
         usersCollection.insertOne(newUser)
         .then((result) => {
@@ -36,16 +50,6 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
             res.send({currentUser : req.body.username});
         })
         .catch((error) => res.status(400).send(error));
-    });
-
-    app.post('/jokes', function(req, res){
-        console.log('Just got a post request to jokes!')
-        const newJoke = {username: req.body.username ,setup : req.body.setup, punchline: req.body.punchline, likedBy : [], dislikedBy : []}
-        jokesCollection.insertOne(newJoke)
-        .then((result) => {
-            res.send()
-        })
-        .catch((error) => res.status(400).send());
     });
 
     app.listen(4001, () => {
